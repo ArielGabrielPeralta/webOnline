fbd5f11b4c15fa578d8282f902abc65e26cb75a7
ArielGabrielPeralta <75574508+ArielGabrielPeralta@users.noreply.github.com>
2021-03-09 09:35:55 -0300

    email verify

--- contactApp/form.py
+++ contactApp/form.py
@@ -3,5 +3,6 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(label="Your name", required=True, max_length=100)
-    email = forms.CharField(label="Email", required=True, max_length=100)
+    email = forms.CharField(label="Email", required=True,
+                            widget=forms.EmailInput(attrs={'type': 'email'}))
     content = forms.CharField(label="Content", widget=forms.Textarea)
